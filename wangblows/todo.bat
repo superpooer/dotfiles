2caf2150e875792febad46678ddee0770caa411d
superpooer <teh.epik.dukk@gmail.com>
2020-06-11 23:12:09 +0100

    more todo

--- wangblows/todo.bat
+++ wangblows/todo.bat
@@ -10,8 +10,7 @@ echo I       l2python for ytviewer                I
 echo I       cmdgfx frog shooting BLANK rp        I
 echo I       temple on spare hdd/thiccpad         I
 echo I       learn latex by answerin q217         I
-echo I       put vimrc on gh n diff               I
-echo I       make asmblr/disasm/remof ollydbg     I
-echo I       put misc projects on gh              I
+echo I       diff vimrc                           I
+echo I       make asmblr/disasm                   I
 echo I                                            I
 echo ----------------------------------------------
