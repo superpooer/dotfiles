022542d725025844dff00d903dea1b0beaa59ac5
superpooer <NicoTAKulmann@gmail.com>
2020-06-12 02:06:33 +0200

    updated and cleaned todo

--- wangblows/todo.bat
+++ wangblows/todo.bat
@@ -1,16 +1,17 @@
 @echo off
 cls
-echo ----------------------------------------------
-echo I                                            I
-echo I       TODO:                                I
-echo I       HUD                                  I
-echo I       CONTINUE book manager                I
-echo I       c shuntyard char only - asm?         I
-echo I       l2python for ytviewer                I
-echo I       cmdgfx frog shooting BLANK rp        I
-echo I       temple on spare hdd/thiccpad         I
-echo I       learn latex by answerin q217         I
-echo I       diff vimrc                           I
-echo I       make asmblr/disasm                   I
-echo I                                            I
-echo ----------------------------------------------
+echo -------------------------------------------
+echo I                                         I
+echo I    TODO:                                I
+echo I    HUD                                  I
+echo I    CONTINUE book manager                I
+echo I    c shuntyard char only - asm?         I
+echo I    l2python for ytviewer                I
+echo I    cmdgfx frog shooting BLANK rp        I
+echo I    temple on spare hdd/thiccpad         I
+echo I    learn latex by answerin q217         I
+echo I    diff vimrc                           I
+echo I    make asmblr/disasm                   I
+echo I    bulkrenamer.bat                      I
+echo I                                         I
+echo -------------------------------------------
