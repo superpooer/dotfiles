a81265c8b8486c2e3f3aa7244e55dfb2ba4b0c7c
superpooer <teh.epik.dukk@gmail.com>
2020-06-13 03:11:14 +0100

    fuck i dont care anymore

--- wangblows/todo.bat
+++ wangblows/todo.bat
@@ -6,7 +6,6 @@ echo I    TODO:                                I
 echo I    HUD                                  I
 echo I    CONTINUE book manager                I
 echo I    c shuntyard char only - asm?         I
-echo I    l2python for ytviewer                I
 echo I    cmdgfx frog shooting BLANK rp        I
 echo I    temple on spare hdd/thiccpad         I
 echo I    learn latex by answerin q217         I
