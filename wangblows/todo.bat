cc3445080d0ff6efed2514a1ac63a43196412e43
superpooer <teh.epik.dukk@gmail.com>
2020-06-14 00:33:07 +0100

    updated todo

--- wangblows/todo.bat
+++ wangblows/todo.bat
@@ -9,7 +9,6 @@ echo I    c shuntyard char only - asm?         I
 echo I    cmdgfx frog shooting BLANK rp        I
 echo I    temple on spare hdd/thiccpad         I
 echo I    learn latex by answerin q217         I
-echo I    diff vimrc                           I
 echo I    make asmblr/disasm                   I
 echo I    bulkrenamer.bat                      I
 echo I    rejig .bats in2 scripts dir          I
